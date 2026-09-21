f5ba7958bc721f5763a90ef1907b666a17bf48e5
mysliwego <kuba.mysliwiec07@gmail.com>
2026-09-21 19:03:14 +0200

    Build Contact page layout

--- app.py
+++ app.py
@@ -18,5 +18,9 @@ def skills():
 def projects():
     return render_template("projects.html")
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=1)
